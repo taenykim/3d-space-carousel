27dfd0e8958df5f21c6cb82fdac30d8f70db04f9
taeeun kim <kimtaeeeny@gmail.com>
2020-10-27 15:56:08 +0900

    [Feat] Add Index page & Main, ImageButton Component

---
+++ components/ImageButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="image-button" :style="{ backgroundColor: color }"></div>
+</template>
+
+<script>
+export default {
+  props: ["color"]
+  //   data() {
+  //     return { color: "red" };
+  //   }
+};
+</script>
+
+<style scoped lang="scss">
+.image-button {
+  flex: 1;
+  margin: 4px 4px 4px 0px;
+
+  &:nth-child(1) {
+    margin: 4px 4px 4px 4px;
+  }
+}
+</style>
---
+++ components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="main">
+    <div class="image-container">
+      <ImageButton v-for="color of colors" :key="color" :color="color" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    colors() {
+      return ["red", "black", "green", "purple", "blue", "orange"];
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/sass/variables.scss";
+
+main {
+  height: calc(100vh - #{$header-height});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-container {
+  display: flex;
+  width: 90%;
+  max-width: 800px;
+  height: 200px;
+  border: 4px solid black;
+}
+</style>
