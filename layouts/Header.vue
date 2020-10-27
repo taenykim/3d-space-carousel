14bfe93c43b05b7832f6c6987bfe7b3dca8501a7
taeeun kim <kimtaeeeny@gmail.com>
2020-10-27 15:55:12 +0900

    [Feat] Add default, header layout

---
+++ layouts/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>헤더</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/sass/variables.scss";
+
+header {
+  height: $header-height;
+  background-color: aqua;
+}
+</style>
