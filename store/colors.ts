2f2142928ef4b9cd0150b2076484a9205d8db814
taeeun kim <kimtaeeeny@gmail.com>
2020-10-29 00:16:15 +0900

    [Feat] 더미데이터 형식 변경

--- store/colors.ts
+++ store/colors.ts
@@ -1,13 +1,40 @@
 const colors = [
-  "red",
-  "black",
-  "green",
-  "purple",
-  "blue",
-  "orange",
-  "dodgerblue",
-  "yellow",
-  "gray"
+  {
+    name: "red",
+    src: "https://images2.alphacoders.com/644/thumb-1920-644457.jpg"
+  },
+  {
+    name: "black",
+    src: "https://images8.alphacoders.com/595/thumb-1920-595000.jpg"
+  },
+  {
+    name: "green",
+    src: "https://images2.alphacoders.com/805/thumb-1920-805177.jpg"
+  },
+  {
+    name: "purple",
+    src: "https://images3.alphacoders.com/749/thumb-1920-749714.jpg"
+  },
+  {
+    name: "blue",
+    src: "https://images2.alphacoders.com/828/thumb-1920-828418.jpg"
+  },
+  {
+    name: "orange",
+    src: "https://images7.alphacoders.com/100/thumb-1920-1008103.png"
+  },
+  {
+    name: "dodgerblue",
+    src: "https://images2.alphacoders.com/822/thumb-1920-822479.jpg"
+  },
+  {
+    name: "yellow",
+    src: "https://images5.alphacoders.com/822/thumb-1920-822471.jpg"
+  },
+  {
+    name: "gray",
+    src: "https://images.alphacoders.com/644/thumb-1920-644450.jpg"
+  }
 ];
 
 export default colors;
