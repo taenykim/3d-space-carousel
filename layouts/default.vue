619dcbd6b0249368d92eb2ca4c981b9a31291a27
taeeun kim <kimtaeeeny@gmail.com>
2020-10-29 19:45:01 +0900

    [Style] 폰트 설정

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,9 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 body {
+  font-family: "Roboto", sans-serif;
   margin: 0;
 }
 #layout {
